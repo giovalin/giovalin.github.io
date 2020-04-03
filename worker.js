27f3f521c6f189b0f5a0e43927f6a304ffd0d4ba
giovalin <55329079+giovalin@users.noreply.github.com>
2020-04-03 17:08:13 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -115,8 +115,10 @@ onmessage = function(e) {
                         if (tmp.includes(campList[i]) ) {campList.splice(i, 1);i--}
                     };
                     if ((e.cartesianLock.length + e.locked.length) < 4 && campList.length == 0) { // can't calculate not enough heroes to fill remaining slots
+                        code = 400;
                         e.risultati = ["not_enough_heroes"];
                     } else if ((e.cartesianLock.length + e.locked.length) > 4) { // Too many locked heroes
+                        code = 400;
                         e.risultati = ["team_size_exceeded"];
                     } else { // can calculate 
                         if ( (e.cartesianLock.length + e.locked.length) > 3 ) campList = ["Ras"]; // placeholder Ras if all heroes are used in multilock or lock-> avoid RangeError
