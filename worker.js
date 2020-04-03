fc1d8b20967200e837fe7cb46702235a91d3ff52
giovalin <55329079+giovalin@users.noreply.github.com>
2020-04-03 16:00:13 +0200

    Add files via upload

--- worker.js
+++ worker.js
@@ -114,41 +114,45 @@ onmessage = function(e) {
                             if (tmp.includes(campList[i]) ) {campList.splice(i, 1);i--}
                         };
                         if ( (e.cartesianLock.length + e.locked.length) < 5 ) {
-                            if ( (e.cartesianLock.length + e.locked.length) > 3 ) campList = ["Ras"]; // placeholder Ras if all heroes are used in multilock -> avoid RangeError
-                            c = printCombos(e.cartesianLock);
-                            c.forEach( (cartesianLocked) => {
-                                    Combinatorics.bigCombination(campList,4-e.locked.length-cartesianLocked.length).forEach(teamComb => {
-                                        var teamComb = teamComb;
-                                        if (e.cartesianLock.length + e.locked.length>3) teamComb = []; // Se locked = 4 allora team deve riportare array vuota
-                                        teamComb = teamComb.concat(cartesianLocked);
-                                        var team = [].concat(teamComb, e.locked);
-                                        let elementoFiltro = teamComb;
-                                        let elementoRisultati = elementoFiltro.map(function (hero, i) { return HeroDB[hero].attribute }).flat();
-                                        let buffsRisultati = elementoFiltro.map(function (hero, i) { return HeroDB[hero].buffs }).flat();
-                                        let debuffsRisultati = elementoFiltro.map(function (hero, i) { return HeroDB[hero].debuffs }).flat();
-                                        let S1debuffsRisultati = elementoFiltro.map(function (hero, i) { return HeroDB[hero].skills[0].debuff }).flat();
-                                        let classeRisultati = elementoFiltro.map(function (hero, i) { return HeroDB[hero].role }).flat();
-                                        let AoE_inTeam = AoEHeroes.some(i => elementoFiltro.includes(i));
-                                        if (e.locked.every(i => team.includes(i)) &&
-                                            e.classe.every(i => classeRisultati.includes(i)) && 
-                                            e.elemento.every(i => elementoRisultati.includes(i)) &&
-                                            e.debuffs.every(i => debuffsRisultati.includes(i)) &&
-                                            e.buffs.every(i => buffsRisultati.includes(i)) &&
-                                            (e.AoE === false || (e.AoE === true && AoE_inTeam )) &&
-                                            (e.noS1debuffs === false || (e.noS1debuffs === true &&  S1debuffsRisultati.filter(function (team) {return team != "20" && team != "25" && team != "21"}).length === 0)) &&
-                                            (e.noDebuffs === false || (e.noDebuffs === true && debuffsRisultati.filter(function (team) {return team != "20" && team != "25" && team != "21"}).length === 0)  )
-                                        ){
-                                            if (e.risultati.length < 200) {
-                                            e.risultati.push(nuovoCampSimulatorTeam2(team));
-                                            e.risultati.sort(function(a, b)  {return ((a.morale > b.morale) ? -1 : ((a.morale == b.morale) ? 0 : 1));});
-                                            } else {
-                                                let risultatoDiQuestoTeam = nuovoCampSimulatorTeam2(team)
-                                                e.risultati.sort(function(a, b) {return ((a.morale > b.morale) ? -1 : ((a.morale == b.morale) ? 0 : 1));});	 
-                                                if  (risultatoDiQuestoTeam.morale > e.risultati[e.risultati.length-1].morale) e.risultati.unshift(risultatoDiQuestoTeam),e.risultati.splice(200);
+                            if ( (e.cartesianLock.length + e.locked.length) > 3 ) campList = ["Ras"]; // placeholder Ras if all heroes are used in multilock or lock-> avoid RangeError
+                            if ((e.cartesianLock.length + e.locked.length) > 4 && campList.length == 0) { // can't calculate not enough heroes to fill remaining slots
+                                console.log("Not enough heroes")
+                            } else { // can calculate 
+                                c = printCombos(e.cartesianLock);
+                                c.forEach( (cartesianLocked) => {
+                                        Combinatorics.bigCombination(campList,4-e.locked.length-cartesianLocked.length).forEach(teamComb => {
+                                            var teamComb = teamComb;
+                                            if (e.cartesianLock.length + e.locked.length>3) teamComb = []; // Se locked = 4 allora team deve riportare array vuota
+                                            teamComb = teamComb.concat(cartesianLocked);
+                                            var team = [].concat(teamComb, e.locked);
+                                            let elementoFiltro = teamComb;
+                                            let elementoRisultati = elementoFiltro.map(function (hero, i) { return HeroDB[hero].attribute }).flat();
+                                            let buffsRisultati = elementoFiltro.map(function (hero, i) { return HeroDB[hero].buffs }).flat();
+                                            let debuffsRisultati = elementoFiltro.map(function (hero, i) { return HeroDB[hero].debuffs }).flat();
+                                            let S1debuffsRisultati = elementoFiltro.map(function (hero, i) { return HeroDB[hero].skills[0].debuff }).flat();
+                                            let classeRisultati = elementoFiltro.map(function (hero, i) { return HeroDB[hero].role }).flat();
+                                            let AoE_inTeam = AoEHeroes.some(i => elementoFiltro.includes(i));
+                                            if (e.locked.every(i => team.includes(i)) &&
+                                                e.classe.every(i => classeRisultati.includes(i)) && 
+                                                e.elemento.every(i => elementoRisultati.includes(i)) &&
+                                                e.debuffs.every(i => debuffsRisultati.includes(i)) &&
+                                                e.buffs.every(i => buffsRisultati.includes(i)) &&
+                                                (e.AoE === false || (e.AoE === true && AoE_inTeam )) &&
+                                                (e.noS1debuffs === false || (e.noS1debuffs === true &&  S1debuffsRisultati.filter(function (team) {return team != "20" && team != "25" && team != "21"}).length === 0)) &&
+                                                (e.noDebuffs === false || (e.noDebuffs === true && debuffsRisultati.filter(function (team) {return team != "20" && team != "25" && team != "21"}).length === 0)  )
+                                            ){
+                                                if (e.risultati.length < 200) {
+                                                e.risultati.push(nuovoCampSimulatorTeam2(team));
+                                                e.risultati.sort(function(a, b)  {return ((a.morale > b.morale) ? -1 : ((a.morale == b.morale) ? 0 : 1));});
+                                                } else {
+                                                    let risultatoDiQuestoTeam = nuovoCampSimulatorTeam2(team)
+                                                    e.risultati.sort(function(a, b) {return ((a.morale > b.morale) ? -1 : ((a.morale == b.morale) ? 0 : 1));});	 
+                                                    if  (risultatoDiQuestoTeam.morale > e.risultati[e.risultati.length-1].morale) e.risultati.unshift(risultatoDiQuestoTeam),e.risultati.splice(200);
+                                                };
                                             };
-                                        };
-                                    });
-                            });
+                                        });
+                                });
+                            };
                         };
                 };
 
