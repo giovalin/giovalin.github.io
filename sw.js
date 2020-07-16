ccff74391187b198b61975fc1e6d21b52172125d
giovalin <55329079+giovalin@users.noreply.github.com>
2020-07-16 22:54:20 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -28,21 +28,31 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-  console.log('Fetch intercepted for:', event.request.url);
-  event.respondWith(caches.match(event.request)
-    .then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        };
-        return fetch(event.request)
-        .then(response => {
-          return caches.open(nomeCache).then(cache => {
-            cache.put(event.request.url, response.clone());
-            return response;
-          });
-         });
-      })
-    );
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request)
+
+      .then(response => {
+        // TODO 5 - Respond with custom 404 page
+        return caches.open(staticCacheName).then(cache => {
+          cache.put(event.request.url, response.clone());
+          return response;
+        });
+      });
+
+    }).catch(error => {
+
+      // TODO 6 - Respond with custom offline page
+
+    })
+  );
 });
 
 
