b003e411f1b32c14e35e438a73373386c0512c70
giovalin <55329079+giovalin@users.noreply.github.com>
2021-02-04 15:27:05 +0100

    Update service-worker.js

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -64,7 +64,7 @@ self.addEventListener('fetch', function(event) {
       
       try {
         return await fetch (event.request)
-        .then(response => { // cache response for next time
+        .then(async response => { // cache response for next time
           if (response.clone().status === 200 || response.clone().status === 304) {
             await caches.open(nomeCache).then(cache => {
               cache.put(event.request.url, response.clone()); 
