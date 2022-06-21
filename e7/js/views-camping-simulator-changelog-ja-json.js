9f0330745e96ff9f844ac347f8c2d26074d087ea
CeciliaBot <giovalin.psn@gmail.com>
2022-06-21 13:56:38 +0200

    E7 test

---
+++ e7/js/views-camping-simulator-changelog-ja-json.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["views-camping-simulator-changelog-ja-json"],{e957:function(o){o.exports=JSON.parse('[[1.1,"24/03/2022",[["added","Changelog localiziation test."]]]]')}}]);
